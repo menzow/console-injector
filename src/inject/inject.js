681efe9dea15d3f55b3454df08c815c4b522f772
Menzo Wijmenga <code@menzo.io>
2018-07-02 22:33:11 +0700

    To prevent hidden/unauthorized calls to the global console.inject method this update will require a user confirm() before injecting any code.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -26,8 +26,15 @@ chrome.extension.sendMessage({}, function(response) {
                 var url = foundLib.latest.replace('http:', 'https:');
                 var libScript =document.createElement('script');
                 libScript.src = url;
-                document.head.appendChild(libScript);
-                return console.log('library injected from ' + url);
+
+                if(confirm(`Please confirm injection of "${foundLib.name}" into context of ${window.location.host}. \nSRC: ${url}`)) {
+                  document.head.appendChild(libScript);
+                  console.log('Requested user confirmation to inject ' + url);
+                } else {
+                  console.error('No confirmation received.. Make sure alerts are allowed for the current tab.')
+                }
+                return;
+
               } else {
                 console.log('library "' + library + '" not found');
               }
